cb51d1c3908ccb428c4d55c215d0b7c2f39e3ef6
superljy <superljy0809@163.com>
2020-08-31 00:44:43 +0800

    Finish.This is the first version

--- hand/js/supremeHand.js
+++ hand/js/supremeHand.js
@@ -3,24 +3,94 @@
     第一次,把在popup端输入的数据传递过去content端,并保存在页面缓存中,页面跳转到相关的类目
     第二次,content页面跳转后,popup端再次发起通信,进行加车操作,读取的是第一次连接时缓存在页面本地的数据
 */
-console.log('Popup');
 
-//获取存储在chrome.storage的数据
-chrome.storage.sync.get({
-    category: null,
-    keyword: null,
-    color: null,
-    size: null,
-    delay: null
-}, (result) => {
-    console.log(result)
-    document.querySelector('#category').value = result.category;
-    document.querySelector('#keyword-input').value = result.keyword;
-    document.querySelector('#color-input').value = result.color;
-    document.querySelector('#size-input').value = result.size;
-    document.querySelector('#delay-input').value = result.delay;
+window.onload = () => {
+    console.log('Popup');
 
-});
+    //选项卡
+    let tabs = document.querySelectorAll('.tab li');
+    let contents = document.querySelectorAll('.content li');
+    document.querySelector('.tab').addEventListener('click', (e) => {
+        let index = 0;
+        for (let i = 0; i < tabs.length; i++) {
+            if (e.target === tabs[i]) {
+                index = i;
+            }
+        }
+        tabs[index].classList.add('tab-active');
+        contents[index].classList.add('content-active');
+        for (tab of tabs) {
+            if (tab !== tabs[index]) {
+                tab.classList.remove('tab-active');
+            }
+        }
+        for (content of contents) {
+            if (content !== contents[index]) {
+                content.classList.remove('content-active');
+            }
+        }
+
+    }, true);
+
+    let inputInfos = document.querySelectorAll('.content input');
+    for (info of inputInfos) {
+        info.addEventListener('blur', () => {
+            chromeStorage();
+        })
+    };
+
+    //获取存储在chrome.storage的数据
+    chrome.storage.sync.get({
+        category: null,
+        keyword: null,
+        color: null,
+        size: null,
+        delay: null,
+        fullname: null,
+        email: null,
+        telephone: null,
+        address: null,
+        address2: null,
+        zip: null,
+        city: null,
+        state: null,
+        country: null,
+        number: null,
+        month: null,
+        year: null,
+        cvv: null
+    }, (result) => {
+        console.log(result)
+        document.querySelector('#category').value = result.category;
+        document.querySelector('#keyword-input').value = result.keyword;
+        document.querySelector('#color-input').value = result.color;
+        document.querySelector('#size-input').value = result.size;
+        document.querySelector('#delay-input').value = result.delay;
+        document.querySelector('#fullname').value = result.fullname;
+        document.querySelector('#email').value = result.email;
+        document.querySelector('#telephone').value = result.telephone;
+        document.querySelector('#address').value = result.address;
+        document.querySelector('#address2').value = result.address2;
+        document.querySelector('#zip').value = result.zip;
+        document.querySelector('#city').value = result.city;
+        document.querySelector('#state').value = result.state;
+        document.querySelector('#country').value = result.country;
+        document.querySelector('#number').value = result.number;
+        document.querySelector('#month').value = result.month;
+        document.querySelector('#year').value = result.year;
+        document.querySelector('#cvv').value = result.cvv;
+
+    });
+
+    //设定好关键字等抢购信息后点击开始运行
+    document.querySelector('#start-btn').addEventListener('click', () => {
+        console.log('Clicked start button');
+        getCustomInfo();
+    }, true);
+
+    //
+}
+
 
 //首字母大写
 let firstUpperCase = ([first, ...rest]) => {
@@ -34,16 +104,14 @@ let getCustomInfo = () => {
         keyword: firstUpperCase(document.querySelector('#keyword-input').value),
         color: firstUpperCase(document.querySelector('#color-input').value),
         size: firstUpperCase(document.querySelector('#size-input').value),
-        delay: document.querySelector('#delay-input').value
+        delay: document.querySelector('#delay-input').value,
+        msgSymbol: 'redirect to category'
     }
 
-    chrome.storage.sync.set(message, () => {
-        console.log('Save')
-    });
-
     /* 
-        使用短连接进行第一次通信,把输入的数据传递过去
-        当接收到content_script返回的信息后,进行下一步addToCart的操作
+        使用短连接进行,把输入的数据传递过去,透过不同的message信息识别不同的通信链接,如这里的msgSymbol
+        第一次的通信把输入的keyword等信息传递到content端并根据输入的分类跳转到对应的类目商品中
+        收到content端回应后执行后续加车操作
     */
     let params = {
         active: true,
@@ -62,26 +130,85 @@ let getCustomInfo = () => {
 }
 
 /*
-    使用长链接通信 避免发起两次短连接造成重复
-    使用两次短连接会导致接收端错误接收信息 如本项目中第一次传递过去的数据会被第二次的覆盖
+    第二次短连接,传递不同的msgSymbol识别不同的通信
+    第二次的通信是要告诉content端进行搜索商品和加入购物车的操作
 */
 let addToCart = () => {
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, (tabs) => {
-        let port = chrome.tabs.connect(tabs[0].id, {
-            name: 'supreme'
-        })
-        port.postMessage('start');
-        port.onMessage.addListener((msg) => {
-            console.log(msg);
+        chrome.tabs.sendMessage(tabs[0].id, {
+            msgSymbol: 'search and add'
+        }, (res) => {
+            console.log(res);
+            checkout();
         })
     })
-
 }
 
-document.querySelector('#start-btn').addEventListener('click', () => {
-    console.log('Clicked start button');
-    getCustomInfo();
-}, true);
+/**
+ * 第三次短连接,传递billing和card的结账信息
+ * 第三次的通信是成功加车后,进入到结账页面,自动填充用户信息输入并结账的流程
+ */
+let checkout = () => {
+    let checkoutInfo = {
+        fullname: document.querySelector('#fullname').value,
+        email: document.querySelector('#email').value,
+        telephone: document.querySelector('#telephone').value,
+        address: document.querySelector('#address').value,
+        address2: document.querySelector('#address2').value,
+        zip: document.querySelector('#zip').value,
+        city: document.querySelector('#city').value,
+        state: document.querySelector('#state').value,
+        country: document.querySelector('#country').value,
+        number: document.querySelector('#number').value,
+        month: document.querySelector('#month').value,
+        year: document.querySelector('#year').value,
+        cvv: document.querySelector('#cvv').value,
+        msgSymbol: 'go checkout'
+    }
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, checkoutInfo, (res) => {
+            console.log(res);
+        })
+    })
+}
+
+chrome.runtime.onMessage.addListener((res, sender, sendResponse) => {
+    if (res.msg === 'checkout') {
+        checkout();
+        sendResponse('checkout done');
+    }
+})
+
+
+//获取billing card等基础信息 并储存到chrome.storage里
+let chromeStorage = () => {
+    let storageInfo = {
+        category: document.querySelector('#category').value,
+        keyword: firstUpperCase(document.querySelector('#keyword-input').value),
+        color: firstUpperCase(document.querySelector('#color-input').value),
+        size: firstUpperCase(document.querySelector('#size-input').value),
+        delay: document.querySelector('#delay-input').value,
+        fullname: document.querySelector('#fullname').value,
+        email: document.querySelector('#email').value,
+        telephone: document.querySelector('#telephone').value,
+        address: document.querySelector('#address').value,
+        address2: document.querySelector('#address2').value,
+        zip: document.querySelector('#zip').value,
+        city: document.querySelector('#city').value,
+        state: document.querySelector('#state').value,
+        country: document.querySelector('#country').value,
+        number: document.querySelector('#number').value,
+        month: document.querySelector('#month').value,
+        year: document.querySelector('#year').value,
+        cvv: document.querySelector('#cvv').value
+    }
+    chrome.storage.sync.set(storageInfo, () => {
+        console.log('Save base infomation.')
+    });
+}
--- js/index.js
+++ js/index.js
@@ -8,34 +8,59 @@ let delay = (time) => {
 }
 
 /*
-    短连接
+    第一次短连接
     接收第一次popup端发送过来的用户输入信息,并在本地缓存起来,方便页面跳转后再次使用同样的信息
     写入缓存并控制跳转 发送response到popup端告知第一步已完成
 */
 chrome.runtime.onMessage.addListener((res, sender, sendResponse) => {
-    console.log(res);
-    window.localStorage.setItem('customInfo', JSON.stringify(res));
-    window.sessionStorage.setItem('customInfo', JSON.stringify(res));
+    if (res.msgSymbol === 'redirect to category') {
+        window.localStorage.setItem('customInfo', JSON.stringify(res));
+        window.sessionStorage.setItem('customInfo', JSON.stringify(res));
 
-    sendResponse(`即将跳转,原链接:${location.href}`);
-    let baseUrl = 'https://www.supremenewyork.com/shop/all/';
-    location.href = baseUrl + res.category;
-})
-
-/*
-    长连接
-    第二次发起的连接,处理因页面发生跳转后数据清空无法只用一次连接完成的剩余部分内容
-*/
-chrome.runtime.onConnect.addListener((port) => {
-    if (port.name === 'supreme') {
-        port.onMessage.addListener(async (res) => {
-            console.log(res);
-            await delay(1000);
-            addToCart();
-        })
+        sendResponse(`redirect done`);
+        let baseUrl = 'https://www.supremenewyork.com/shop/all/';
+        location.href = baseUrl + res.category;
     }
 })
 
+/*
+    第二次短连接
+    第二次发起的连接,判断msgSymbol,进行商品搜索和加车操作
+*/
+chrome.runtime.onMessage.addListener((res, sender, sendResponse) => {
+    if (res.msgSymbol === 'search and add') {
+        addToCart();
+        sendResponse('start to add');
+    }
+})
+
+/**
+ * 加入购物车后,判断当前页面是否到达结账页面
+ * 如已到达则发信息通知popup端执行checkout的步骤
+ * (content端可用的chrome api只有onMessage sendMessage这两种,直接使用runtime.sendMessage发送消息 在popup端监听即可)
+ */
+if (location.href === 'https://www.supremenewyork.com/checkout') {
+    chrome.runtime.sendMessage({
+        msg: 'checkout'
+    }, (res) => {
+        console.log(res);
+    })
+}
+
+/**
+ * 第三次短连接
+ * 第三次通信是加车成功后进行结账的操作
+ */
+chrome.runtime.onMessage.addListener((res, sender, sendResponse) => {
+    if (res.msgSymbol === 'go checkout') {
+        sendResponse('try checkout');
+        checkout(res);
+    }
+})
+
+/**
+ * 选择分类跳转后添加购物车操作
+ */
 let addToCart = async () => {
     let customInfo = JSON.parse(window.localStorage.customInfo);
     if (location.href === 'https://www.supremenewyork.com/shop/all/' + customInfo.category) {
@@ -46,16 +71,7 @@ let addToCart = async () => {
 
         //进入商品后获取尺码选项 选中尺码并加车
         await delay(parseInt(customInfo.delay));
-
         let productSizeSelectArr = document.querySelectorAll('select#s option');
-
-        // if (soldOut) {
-        //     console.log('Product Sold-out');
-        //     location.reload();
-        //     addToCart();
-        // } else {
-        //     selectSize(productSizeSelectArr, customInfo);
-        // }
         selectSize(productSizeSelectArr, customInfo);
 
         //加车后点击进入结账页面
@@ -65,6 +81,12 @@ let addToCart = async () => {
 
 }
 
+/**
+ * 判断关键字与颜色 选择商品方法
+ * @param {页面中所有商品的数组} productArr 
+ * @param {对应商品颜色的数组} colorArr 
+ * @param {自定义关键字 颜色 尺码信息} customInfo 
+ */
 let selectProduct = (productArr, colorArr, customInfo) => {
     for (let i = 0; i < productArr.length; i++) {
         if (productArr[i].innerHTML.indexOf(customInfo.keyword) !== -1 && colorArr[i].innerHTML.indexOf(customInfo.color) !== -1) {
@@ -74,10 +96,15 @@ let selectProduct = (productArr, colorArr, customInfo) => {
     }
 }
 
+/**
+ * 判断尺码并进行加车方法 中间会判断商品是否已售罄
+ * @param {尺码数组} sizeArr 
+ * @param {自定义关键字 颜色 尺码信息} customInfo 
+ */
 let selectSize = (sizeArr, customInfo) => {
     let soldOut = document.querySelector('#add-remove-buttons b.sold-out');
     if (soldOut) {
-        alert('Product Sold-out,please retry..');
+        alert('Product Sold-out,please retry or change product..');
     }
     for (let i = 0; i < sizeArr.length; i++) {
         if (sizeArr[i].innerHTML.indexOf(customInfo.size) !== -1) {
@@ -86,4 +113,26 @@ let selectSize = (sizeArr, customInfo) => {
         }
     }
     document.querySelector('#add-remove-buttons input').click();
+}
+//结账方法
+let checkout = (checkoutInfo) => {
+    console.log(checkoutInfo);
+    document.querySelector('#order_billing_name').value = checkoutInfo.fullname;
+    document.querySelector('#order_email').value = checkoutInfo.email;
+    document.querySelector('#order_tel').value = checkoutInfo.telephone;
+    document.querySelector('#bo').value = checkoutInfo.address;
+    document.querySelector('#oba3').value = checkoutInfo.address2;
+    document.querySelector('#order_billing_zip').value = checkoutInfo.zip;
+    document.querySelector('#order_billing_city').value = checkoutInfo.city;
+    document.querySelector('#order_billing_state').value = checkoutInfo.state;
+    document.querySelector('#order_billing_country').value = checkoutInfo.country;
+    document.querySelector('#credit_card_type').value = 'credit card';
+    document.querySelector('#rnsnckrn').value = checkoutInfo.number;
+    document.querySelector('#credit_card_month').value = checkoutInfo.month;
+    document.querySelector('#credit_card_year').value = checkoutInfo.year;
+    document.querySelector('#orcer').value = checkoutInfo.cvv;
+    document.querySelector('#order_terms').checked = true;
+    setTimeout(() => {
+        document.querySelector('#pay input.button').click();
+    }, 500);
 }
